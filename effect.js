0337eca744ba00c567f1668fbf1923130dd8b8ba
Guilherme <Fireguiquenbr@gmail.com>
2023-04-21 21:08:45 -0300

    Update

--- effect.js
+++ effect.js
@@ -2,10 +2,10 @@ const mainElement = document.querySelector('#celestialBody');
 
 mainElement.addEventListener('click', function (){
     if (mainElement.classList.contains('move-left')){
-        mainElement.classList.remove('move-left');
-        mainElement.classList.add('move-right')
-    } else{
         mainElement.classList.remove('move-right');
-        mainElement.classList.add('move-left')    
+        mainElement.classList.add('move-left')
+    } else{
+        mainElement.classList.remove('move-left');
+        mainElement.classList.add('move-right')    
     }
 });

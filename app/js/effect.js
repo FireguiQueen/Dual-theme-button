e6c4f8bace4619812f1f91954ff4bf6badfdbca5
Guilherme <Fireguiquenbr@gmail.com>
2024-04-08 21:29:45 -0300

    update

--- app/js/effect.js
+++ app/js/effect.js
@@ -1,14 +1,22 @@
 const mainElement = document.querySelector('#celestialBody');
+const backgroundPage = document.querySelector('.bc-page');
 
 const whole = document.querySelectorAll('.whole');
 const cloud = document.querySelectorAll('.cloud');
 const stars = document.querySelectorAll('.star');
 const background = document.querySelector('#base');
 
-mainElement.addEventListener('click', function (){
-    if (mainElement.classList.contains('move-right')){
+mainElement.addEventListener('click', function()
+{
+    if (backgroundPage.classList.contains('white'))
+    {
+        backgroundPage.classList.toggle('dark');
+    }
+    if (mainElement.classList.contains('move-right'))
+    {
         mainElement.classList.remove('move-right');
         mainElement.classList.add('move-left')
+
         
         whole.forEach(e => {
             e.style.backgroundColor = 'transparent'
@@ -22,24 +30,28 @@ mainElement.addEventListener('click', function (){
 
         base.style.backgroundColor = '#5494cc'
         base.style.transition = 'background-color linear 200ms'
-
-        
-    } else{
-        cloud.forEach(e => {
+    }
+    else
+    {
+        cloud.forEach(e =>
+        {
             e.style.bottom = '-600px'
             e.style.transition = 'bottom linear 2s'
         })
 
-        whole.forEach(e => {
+        whole.forEach(e =>
+        {
             e.style.backgroundColor = '#949eb2'
             e.style.transition = 'background-color linear 300ms'
-        })        
+        })
+
         base.style.backgroundColor = '#1d1e2d'
         base.style.transition = 'background-color linear 200ms'
         mainElement.classList.remove('move-left');
         mainElement.classList.add('move-right');
         
-        stars.forEach(e =>{
+        stars.forEach(e =>
+        {
             e.style.backgroundColor = 'white';
         })
     }
